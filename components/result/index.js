829e620ff31f3ea9135e9db869a653dc18316d6d
fport <furkanportakalx@gmail.com>
2022-01-09 01:58:39 +0300

    feat: add result page

--- components/result/index.js
+++ components/result/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './index.module.css';
-import {useCopyToClipboard} from '@utils'
+import { useCopyToClipboard } from '@utils';
+import Link from 'next/link';
 
 /* eslint-disable react/prop-types */
 export default function Info({ data }) {
- const [isCopied, copyToClipboard] = useCopyToClipboard({
+  const [isCopied, copyToClipboard] = useCopyToClipboard({
     debug: true,
     message: 'Press #{x} to copy',
     interval: 3000,
@@ -13,9 +14,13 @@ export default function Info({ data }) {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.link} onClick={() => copyToClipboard(data)}>{data}</div>
+        <div className={styles.link} onClick={() => copyToClipboard(data)}>
+          {data}
+        </div>
         <span className={styles.copy}>{isCopied && 'kopyalandi'}</span>
-        <button className={styles.btn}>Get Document</button>
+        <Link href="/result">
+          <a className={styles.btn}>Get Document</a>
+        </Link>
       </div>
     </div>
   );
---
+++ pages/result.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Video from '@c/video';
+export default function Result() {
+  return (
+    <div>
+      <Video />
+    </div>
+  );
+}
